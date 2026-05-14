9f9352d37d0556407075b7ce03527cf155c36f3b
AadilWani2 <waniaadil211@gmail.com>
2026-05-15 02:17:50 +0530

    updated final

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -12,7 +12,7 @@ const userRouter = require("./routes/user.routes")
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true
 }))
 app.use("/api/auth", authRouter)
